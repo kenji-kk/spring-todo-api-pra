2ab2db7dc754ff55c8f1681ffe900464a6e850ed
kenji-kk <kenjix12x22@gmail.com>
2023-07-16 19:55:16 +0900

    create Repository,Record

--- src/main/java/com/example/todoapi/service/sample/SampleService.java
+++ src/main/java/com/example/todoapi/service/sample/SampleService.java
@@ -1,11 +1,18 @@
 package com.example.todoapi.service.sample;
 
+import com.example.todoapi.repository.sample.SampleRecord;
+import com.example.todoapi.repository.sample.SampleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service // Bean(DI管理対象クラス)登録
+@RequiredArgsConstructor
 public class SampleService {
 
+    private final SampleRepository repository;
+
     public SampleEntity find() {
-        return new SampleEntity("Hello");
+        SampleRecord record = repository.select();
+        return new SampleEntity(record.getContent());
     }
 }
